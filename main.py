10740bdb2da29bb0658e9eed94bbb500edef1998
mrvarbik25 <mr.varbik25@gmail.com>
2018-10-27 23:29:37 +0300

    The breakdown is rendered into a separate function and the method of checking the user's choice is changed.

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-## TODO: сделать процентное соотношение
 with_out_turns = [] # обявление списков и констант
 turns = []
 template = 'Кол-во повторений: {0}\nПовторения: {1}'
 text = []
+choice = None
+
+def split(string):
+    """разбивка строки или файла"""
+    for i in string: # перебор словаря 'x' со всеми словами
+        if i not in with_out_turns: # если i нет в списке with_out_turns => записываем её туда
+            with_out_turns.append(i)
+        elif i in with_out_turns:   # если i есть в списке with_out_turns => записываем её в список "повторений"
+            turns.append(i)
 
 def write_in_file(choice, content): # запись в файл
     """функция записи в файл того что было передано в аргумент content"""
@@ -27,11 +35,7 @@ def split_string():
     """разбивает строку по символу разделителю"""
     string = input() # ввод строки
     string = string.split(choice_simvol_separator) # разбивка в словарь по символу разделителю
-    for i in string: # перебор словаря 'c' со всеми словами
-        if i not in with_out_turns: # если i нет в списке with_out_turns => записываем её туда
-            with_out_turns.append(i)
-        elif i in with_out_turns:   # если i есть в списке with_out_turns => записываем её в список "повторений"
-            turns.append(i)
+    split(string)   # разбивка текста из переменной string в словарь по символу разделителю
     if turns == []:                 # если список turns пустой то выводим "повторений нет"
         print('\nПовторений нет!')
     else:                           # иначе выводим: Повторения кол-во повторений, Без повторений
@@ -46,11 +50,7 @@ def split_file():
     text = file.read()  # чтение содержимого файла в переменную text
     file.close()
     text = text.split(choice_simvol_separator) # разбивка текста из переменной text в словарь по символу разделителю
-    for i in text:  # перебор словаря 'text' со всеми словами
-        if i not in with_out_turns: # если i нет в списке with_out_turns => записываем её туда
-            with_out_turns.append(i)
-        elif i in with_out_turns:   # если i есть в списке with_out_turns => записываем её в список "повторений"
-            turns.append(i)
+    split(text) # вызов функции для разбивки переменной 'text' по символу разделителю в словарь
     write_in_file_choice = str(input('Записать повторения в файл?[yes/no]: '))  # спрашивает пользователя хочет ли он записать повторения в файл
     write_in_file(write_in_file_choice, template.format(len(turns), str(turns))) # запись в файл по шаблону и подстановка данных через метод format если пользователь ответил YES
     print_content(write_in_file_choice, template.format(len(turns), str(turns))) # вывод на экран по шаблону и подстановка данных через метод format если пользователь ответил NO
@@ -58,16 +58,17 @@ def split_file():
 def ifchoice(choice):
     """проверка choice == 1 или 2
     и вызов соответствующей строки"""
-    if choice == 1: # чтение строки
-        split_string()
-    elif choice == 2:   # чтение файла
-        split_file()
+    while choice == None:   # пока choice = None, если пользователь ввел НЕ 1 ИЛИ 2 а другое число тогда выход из программы
+        choice = int(input('\nВыберите:\n1 - чтение строки\n2 - чтение файла\n>>> '))
+        if choice == 1:     # чтение строки
+            split_string()
+        elif choice == 2:   # чтение файла
+            split_file()
 
 def welcome():
     """приветствие"""
-    print('\nДобро пожаловать в программу "Повторения" \tauthor Yehor Varbanskiy.\nВведите символ разделитель.\nПример:\'\' или \',\'\n')
+    print('\nДобро пожаловать в программу "Повторения" \tauthor Yehor Varbanskiy.\nВведите символ разделитель.\nПример:\' \' или \',\'\n')
 
 welcome()
 choice_simvol_separator = str(input('Выберите сивол разделитель: '))    # ввод символ разделитель
-choice = int(input('Выберите:\n1 - чтение строки\n2 - чтение файла\n>>> ')) # выбор чтение строки или файла
 ifchoice(choice)
