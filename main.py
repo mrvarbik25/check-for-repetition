6287f888b37d31eddfcb000a53095ed72de4aead
Yehor <mr.varbik25@gmail.com>
2018-11-01 15:49:10 +0200

    added ability to work through arguments.

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
+# program to check for repetition.
+# example to run with arguments: python3 main.py x.txt space no
+import sys
+import os
+template = 'Кол-во повторений: {0}\nПовторения: {1}'
 with_out_turns = [] # обявление списков и констант
 turns = []
-template = 'Кол-во повторений: {0}\nПовторения: {1}'
 text = []
 choice_simvol_separator = None
 file_name = None
+
+def work_with_args():
+    args = sys.argv[1:]
+    if args[1] == 'space':  # если во втором аргументе указано 'space' то заменить это на символ пробела ' '
+        args.pop(1)
+        args.insert(1, ' ')
+    if args:    # если в аргументы что то передано
+        if os.path.exists(args[0]): # если указан верный путь к файлу
+            try:
+                file_name = args[0] # присвоить к переменной file_name значение первого аргумента
+                with open(file_name) as file:  # открыть файл для чтения
+                    text = file.read()
+                    text = text.split(args[1])
+                    sort_function(text) # сортировка на повторения
+                    if args[2] == 'no': # если третий аргумент = no, просто вывести на экран
+                        print_content('no', template.format(len(turns), str(turns)))
+                    if args[2] == 'yes':    # если третий аргумент = yes, то записать в файл с повторениями
+                        if '/' in args[0]:    # если указан путь к файлу
+                            index = args[0].rfind('/')    # записываем индекс самого правого символа '/'
+                            file_with_repetitions = args[0][:index + 1] + 'file_with_repetitions.txt' # и посредством среза узнаем путь к файлу а посредством конкатенации добавляем имя file_with_repetitions.txt и записываем файл с повторениями туда (/some/path/file_with_repetitions.txt)
+                            with open(file_with_repetitions, 'w') as file:  #  создание файла
+                                file.write(template.format(len(turns), str(turns))) # записать в файл шаблон
+                                print('Повторения записаны в файл \'file_with_repetitions.txt\'')
+                        else:   # если указано имя файла а не путь к нему
+                            with open('file_with_repetitions.txt', 'w') as file:  #  создание файла
+                                file.write(template.format(len(turns), str(turns))) # запись в файл temlpate
+                            print('Повторения записаны в файл \'file_with_repetitions.txt\'')
+            except IndexError:  # если переданы не все аргументы
+                print('[ Error ] [ NotAllArgumentsSpecified ]')
+                print('Try this: python3 main.py path_to_file space [write repetitions to file? or just display? yes / no]')
+        else:   # если указан неверный путь к файлу
+            print('[ file not found ]')
 
 def sort_function(string):
     """сортировка строки или файла в списки"""
@@ -89,4 +125,6 @@ def main():
     ifchoice()
 
 if __name__ == '__main__':
-    main()
+    work_with_args()
+    if sys.argv[1:] == []:  # если аргументов нет тогда обычный запуск
+        main()
